b09e9b84ea22b964eda439b20a1a4a53a3a8d518
JoseAntonioSeguraGomez <115014384+JoseAntonioSegura@users.noreply.github.com>
2024-06-11 13:44:54 +0200

    3b, bueno bonito y barato

--- src/public/averageCategories.js
+++ src/public/averageCategories.js
@@ -4,7 +4,7 @@ console.log('Respuestas del localStorage: ');
 console.log(answersObject2);
 
 let categoriesContainer2 = document.getElementById('categoriesContainer');
-
+let imgLoading2 = document.getElementById('loading2');
 if (answersObject2 && categoriesContainer2) {
     // Pasar por cada categoría
     for (let key in answersObject2) {
@@ -16,24 +16,25 @@ if (answersObject2 && categoriesContainer2) {
         categoryContainer.appendChild(categoryName);
 
         let categoryScore = document.createElement('h1');
-        categoryScore.innerHTML = answersObject2[key].toFixed(2);
-        //si es NaN o null, convertirlo a 5
-        
+        let scoreValue = answersObject2[key] ? answersObject2[key].toFixed(2) : 5;
+        categoryScore.innerHTML = scoreValue;
+
         categoryContainer.appendChild(categoryScore);
 
-        if (categoryScore.innerHTML >= 0 && categoryScore.innerHTML <= 3) {
+        if (scoreValue >= 0 && scoreValue <= 3) {
             categoryContainer.style.backgroundColor = '#F79394';
             categoryContainer.style.color = '#C60001';
-        }
-        if (categoryScore.innerHTML > 3 && categoryScore.innerHTML <= 4) {
+        } else if (scoreValue > 3 && scoreValue <= 4) {
             categoryContainer.style.backgroundColor = '#FDF6C4';
             categoryContainer.style.color = '#F74B00';
-        }
-        if (categoryScore.innerHTML > 4 && categoryScore.innerHTML <= 5) {
+        } else if (scoreValue > 4 && scoreValue <= 5) {
             categoryContainer.style.backgroundColor = '#8BE68B';
             categoryContainer.style.color = '#012E1F';
         }
 
+        imgLoading2.style.display = 'none';
+
         categoriesContainer2.appendChild(categoryContainer);
     }
 }
+
